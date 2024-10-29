2780653666c388249d424ed8a74cf8146fc34a53
Shweta Karne <ShwetaKarne84@gmail.com>
2024-10-29 15:39:24 -0700

    Add input integer limit

--- Exponent Table/Program.cs
+++ Exponent Table/Program.cs
@@ -16,7 +16,8 @@
                     isValidNumber = int.TryParse(Console.ReadLine(), out inputNumber); //exponents will be displayed till the number entered.
 
                     //Any non-numeric value or value less than or equal to 0 will be considered as invalid input
-                    if (!isValidNumber || inputNumber <= 0 ) 
+                    // Also highest value integer can hold is 2147483647, hence any input greater than this will be considered as invalid
+                    if (!isValidNumber || inputNumber <= 0 || inputNumber > 2147483647) 
                     { 
                         Console.WriteLine("Invalid number!\n");
                         isValidNumber = false;
@@ -27,6 +28,7 @@
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("\nNumber\t\tSquared\t\tCubed");
 
+                
                 for(int counter = 1; counter  <= inputNumber; counter++) //Loop until all the exponents are displayed.
                 {
                     //display the square and cube of a number using Math.Pow() function.

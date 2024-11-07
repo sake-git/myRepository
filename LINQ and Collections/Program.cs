9823a4fc1c68bd7850c8d72d6c2705a252ab49ea
Shweta Karne <ShwetaKarne84@gmail.com>
2024-11-07 11:42:09 -0800

    initial commit

---
+++ LINQ and Collections/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace LINQ_and_Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 10, 2330, 112233, 12, 949, 3764, 2942, 523863 };
+            Console.WriteLine("Array Content:  \t");
+            foreach( int num in nums)
+            {
+                Console.Write(num + " ");
+            }
+            do
+            {
+                Console.WriteLine("\n\n*************************************************");
+                Console.WriteLine("\t\tMenu");
+                Console.WriteLine("*************************************************");
+                Console.WriteLine("1. Find the Minimum value");
+                Console.WriteLine("2. Find the Maximum value");
+                Console.WriteLine("3. Find the Maximum value less than 10000");
+                Console.WriteLine("4. Find all the values between 10 and 100");
+                Console.WriteLine("5. Find all the Values between 100000 and 999999 inclusive");
+                Console.WriteLine("6. Count all the even numbers");
+                Console.WriteLine("*************************************************\n");
+                Console.WriteLine("Enter your choice");
+                string choice = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Green;
+                switch (choice)
+                {
+                    case "1":
+                        int min = nums.Min();
+                        Console.WriteLine("Minimum value is {0}", min);
+                        break;
+                    case "2":
+                        int? max = nums.Max();
+                        Console.WriteLine("Maximum value is {0}", max);
+                        break;
+                    case "3":
+                        max = nums.Max(x => x < 10000? x: null);
+                        Console.WriteLine("Maximum value less than 10000 is {0}", max);
+                        break;
+                    case "4":
+                        IEnumerable<int> filteredNums = nums.Where(x => x > 10 && x < 100);
+                        foreach(int num in filteredNums)
+                        {
+                            Console.Write(num + " ");
+                        }
+                        break;
+                    case "5":
+                        filteredNums = nums.Where(x => x >= 100000 && x <= 999999);
+                        foreach (int num in filteredNums)
+                        {
+                            Console.Write(num + " ");
+                        }
+                        break;
+                    case "6":
+                        filteredNums = nums.Where(x => x % 2 == 0);
+                        foreach (int num in filteredNums)
+                        {
+                            Console.Write(num + " ");
+                        }
+                        break;
+                    case "0":
+                        Console.WriteLine("Goodbye!");
+                        Console.ReadKey();
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please select a correct option:");
+                        break;
+                }
+                Console.ForegroundColor = ConsoleColor.Gray;
+            } while (true);
+        }
+    }
+}
---
+++ Shopping List/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Shopping_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,double> catalogue = new Dictionary<string, double>()
+            {
+                {"KitKat", 1.0 },
+                {"Water", 2.0 },
+                {"Coke",1.5 },
+                {"Lays",2.0 },
+                {"Pepsi",1.0 },
+                {"Mint", 0.5 }
+            };
+
+            List<string> shoppingList = new List<string>();
+            Console.WriteLine("Item Catalogue");
+            do
+            {
+                
+                int count = 0;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nItem\t\tPrice");
+                Console.WriteLine("************************************");
+                foreach (KeyValuePair<string, double> kvp in catalogue)
+                {
+                    Console.WriteLine($"{++count}. {kvp.Key}  ${kvp.Value}");
+                }
+                Console.WriteLine("************************************\n");
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+                Console.WriteLine("What item would you like to order?");
+                string item = Console.ReadLine();
+                bool isValid = catalogue.ContainsKey(item.Trim());
+                if (isValid)
+                {
+                    shoppingList.Add(item.Trim());
+                    Console.WriteLine($"Adding {item} to cart at ${catalogue[item]}");
+
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, we don't have those.  Please try again. ");
+                }
+                Console.WriteLine("\nWould you like to order anything else (y/n)?");
+            }while(Console.ReadLine().ToLower() == "y");
+
+            Console.WriteLine("\nThanks for your order!");
+            Console.WriteLine("Here's what you got:");
+            Console.ForegroundColor = ConsoleColor.Green;
+            double sum = 0.0;
+            foreach (string item in shoppingList)
+            {
+                Console.WriteLine($"{item} \t\t ${catalogue[item]}");
+                sum = sum + catalogue[item];
+            }
+
+            double average = sum / shoppingList.Count;
+            Console.WriteLine($"\nAverage price per item in order was ${average}");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
